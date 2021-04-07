391c427048834fac3b4a1abf953e2a87fdc730fc
「shєrl✪cʞ」 <72887386+Mr-SHRLCK@users.noreply.github.com>
2021-04-07 22:15:30 +0530

    Yo 🥳

--- SaitamaRobot/modules/aifunctions.py
+++ SaitamaRobot/modules/aifunctions.py
@@ -1,13 +1,13 @@
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from MashaRoBot import telethn, OWNER_ID
-from MashaRoBot.events import register
+from SaithamaRobot import telethn, OWNER_ID
+from SaitamaRobot.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
 import asyncio, os
 
-@register(pattern="Masha (.*)")
+@register(pattern="Jarvis (.*)")
 async def hmm(event):
   test = event.pattern_match.group(1)
   r = ('\n    \"consent\": true,\n    \"ip\": \"::1\",\n    \"question\": \"{}\"\n').format(test)
@@ -34,7 +34,7 @@ async def hmm(event):
    except CFError as e:
            print(e)
   elif "ann" in result:
-   pro = "My name is Masha"
+   pro = "My name is Jarvis"
    try:
       async with telethn.action(event.chat_id, 'typing'):
            await asyncio.sleep(2)
