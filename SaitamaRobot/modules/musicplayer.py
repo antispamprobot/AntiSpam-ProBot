0931918b2bac4074cd9574af10dcde00b7976163
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-07 20:16:57 +0530

    Update musicplayer.py

--- SaitamaRobot/modules/musicplayer.py
+++ SaitamaRobot/modules/musicplayer.py
@@ -1,20 +1,23 @@
 __mod_name__ = "Music Player"
 __help__ = """
-<b> VC PLAYER </b>
-VC player plays plays music in your group's voice chat
+MUSIC PLAYER 
+Music player which helps you to plays music in your group's voice chat
 
-<b> Setting up </b>
+ Setting up
 1) Add @JARVISMUSICASSITANT to your group
 2) Make bot admin
 3) Start a voice chat
 
-<b> Commands </b>
+ Commands 
 - /play [link/ reply: mp3 file or yt link] : Play/Add to queue of given song
 - /pause : Pause music play (Admin only)
-- /ytp : To play a song from youtube(THIS MAY CAUSE ERRORS SO IF U SEE ANY ERROR REPORT IT @JARVISUPPORT
 - /skip : Skips current song (Admin only)
 - /end : End the music play (Admin only)
-- /seaech : To search a particular song and returns the youtube link for it.
+-/search : To search a particular song on youtube
 
-<b> PLEASE NOTE THIS IS HEAVILY UNSTABLE AND CAN BE STOPPED ANYTIME </b>
+
+
+‼️‼️ PLEASE NOTE THIS IS HEAVILY UNSTABLE AND CAN BE STOPPED ANYTIME ‼️‼️
 """
+
+
