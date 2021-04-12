8755216925fb0a7087941357dd6e3d32a3268be4
IronMan <73984830+Uday0011@users.noreply.github.com>
2021-04-12 10:41:42 +0530

    Update karma.py

--- SaitamaRobot/modules/karma.py
+++ SaitamaRobot/modules/karma.py
@@ -101,7 +101,7 @@ async def karma(_, message):
             if limit > 9:
                 break
             try:
-                user_name = (await telethn.get_users(int(user_idd))).username
+                user_name = (await pgram.get_users(int(user_idd))).username
             except Exception:
                 continue
             msg += f"{user_name} : `{karma_count}`\n"
