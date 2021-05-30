5cbca0e61d182e016994a9a06683f5bb38384ed7
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-05-30 15:21:52 +0530

    Update animation.py

--- SaitamaRobot/modules/animation.py
+++ SaitamaRobot/modules/animation.py
@@ -9,11 +9,16 @@ from telegram.ext import CallbackContext, run_async
 #sleep how many times after each edit in 'onichan'
 EDIT_SLEEP = 2
 #edit how many times in 'onichan'
-EDIT_TIMES = 3
+EDIT_TIMES = 5
 
 POLICE_SIREN = [
     "🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵",
     "🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴"
+]
+
+LOVE_EMOJY = [
+    "❤️🧡❤️💚❤️💙❤️💜\n❤️💜❤️💙❤️💚❤️🧡\n❤️🧡❤️💚❤️💙❤️💜",
+    "💜❤️💜💙💜💚💜🧡\n🧡❤️💚❤️💙❤️💜❤️\n💜❤️💙❤️💚❤️🧡❤️"
 ]
 
 
@@ -26,14 +31,25 @@ def onichan(update: Update, context: CallbackContext):
         time.sleep(EDIT_SLEEP)
     msg.edit_text('onichan , you are under arrest!')
 
+@run_async
+def love(update: Update, context: CallbackContext):
+    msg = update.effective_message.reply_text('checking love')
+    for x in range(EDIT_TIMES):
+        msg.edit_text(LOVE_EMOJY[x % 2])
+        time.sleep(EDIT_SLEEP)
+    msg.edit_text('True Love!')
+
 
 __help__ = """
 • `/onichan`*:* Sends a police to arrest your onichan. 
+• `/love`*:* Checks Love in your heart weather it's true or fake.
 """
 
 ONICHAN_HANDLER = DisableAbleCommandHandler("onichan", onichan)
+LOVE_HANDLER = DisableAbleCommandHandler ("love", love)
 dispatcher.add_handler(ONICHAN_HANDLER)
+dispatcher.add_handler(LOVE_HANDLER)
 
 __mod_name__ = "Animation"
-__command_list__ = ["onichan"]
-__handlers__ = [ONICHAN_HANDLER]
+__command_list__ = ["onichan", "love"]
+__handlers__ = [ONICHAN_HANDLER , LOVE_HANDLER]
