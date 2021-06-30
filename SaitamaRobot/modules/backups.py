6d25e8d7a5e14472fda1b5477187c66eafac66e5
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-06-30 13:41:36 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -367,15 +367,15 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backups"
+__mod_name__ = "🄱🄰🄲🄺🅄🄿🅂"
 
 __help__ = """
 *Only for group owner:*
 
- • `/import`: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ • /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- • `/export`: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ • /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
