df6b3c6a5b96cb1555c0bf113309930a58b44202
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-06-30 16:00:11 +0530

    Update warns.py

--- SaitamaRobot/modules/warns.py
+++ SaitamaRobot/modules/warns.py
@@ -463,20 +463,20 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/warns <userhandle>`*:* get a user's number, and reason, of warns.
- • `/warnlist`*:* list of all current warning filters
+ • /warns <userhandle>*:* get a user's number, and reason, of warns.
+ • /warnlist*:* list of all current warning filters
 
 *Admins only:*
- • `/warn <userhandle>`*:* warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
- • `/resetwarn <userhandle>`*:* reset the warns for a user. Can also be used as a reply.
- • `/addwarn <keyword> <reply message>`*:* set a warning filter on a certain keyword. If you want your keyword to \
+ • /warn <userhandle>*:* warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+ • /resetwarn <userhandle>*:* reset the warns for a user. Can also be used as a reply.
+ • /addwarn <keyword> <reply message>*:* set a warning filter on a certain keyword. If you want your keyword to \
 be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`. 
- • `/nowarn <keyword>`*:* stop a warning filter
- • `/warnlimit <num>`*:* set the warning limit
- • `/strongwarn <on/yes/off/no>`*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
+ • /nowarn <keyword>*:* stop a warning filter
+ • /warnlimit <num>*:* set the warning limit
+ • /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
-__mod_name__ = "Warnings"
+__mod_name__ = "🅆🄰🅁🄽🅂"
 
 WARN_HANDLER = CommandHandler("warn", warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(["resetwarn", "resetwarns"],
