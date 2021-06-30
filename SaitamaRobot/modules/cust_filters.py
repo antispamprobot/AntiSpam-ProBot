e72d58aa19a86854749e8a9670bb79abade40e39
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-06-30 14:32:42 +0530

    Update cust_filters.py

--- SaitamaRobot/modules/cust_filters.py
+++ SaitamaRobot/modules/cust_filters.py
@@ -587,32 +587,32 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/filters`*:* List all active filters saved in the chat.
+ • /filters*:* List all active filters saved in the chat.
 
 *Admin only:*
- • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+ • /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
- Separate diff replies by `%%%` to get random replies
+ Separate diff replies by *%%%* to get random replies
  *Example:* 
- `/filter "filtername"
+ /filter "filtername"
  Reply 1
  %%%
  Reply 2
  %%%
- Reply 3`
- • `/stop <filter keyword>`*:* Stop that filter.
+ Reply 3
+ • /stop <filter keyword>*:* Stop that filter.
 
 *Chat creator only:*
- • `/removeallfilters`*:* Remove all chat filters at once.
+ • /removeallfilters*:* Remove all chat filters at once.
 
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check `/markdownhelp` to know more!
+Check /markdownhelp to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "🄵🄸🄻🅃🄴🅁🅂"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
