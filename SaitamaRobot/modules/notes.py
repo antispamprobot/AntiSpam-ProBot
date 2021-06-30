d36983a03b5245f89d661e8db2f7f7f41ce5b186
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-06-30 15:18:03 +0530

    Update notes.py

--- SaitamaRobot/modules/notes.py
+++ SaitamaRobot/modules/notes.py
@@ -477,25 +477,25 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/get <notename>`*:* get the note with this notename
- • `#<notename>`*:* same as /get
- • `/notes` or `/saved`*:* list all saved notes in this chat
- • `/number` *:* Will pull the note of that number in the list. 
+ • /get <notename>*:* get the note with this notename
+ • #<notename>*:* same as /get
+ • /notes or /saved*:* list all saved notes in this chat
+ • /number *:* Will pull the note of that number in the list. 
 If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
 be useful when updating a current note.
 
 *Admins only:*
- • `/save <notename> <notedata>`*:* saves notedata as a note with name notename
+ • /save <notename> <notedata>*:* saves notedata as a note with name notename
 A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
 `buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info.
- • `/save <notename>`*:* save the replied message as a note with name notename
- • `/clear <notename>`*:* clear note with this name
- • `/removeallnotes`*:* removes all notes from the group
+ • /save <notename>*:* save the replied message as a note with name notename
+ • /clear <notename>*:* clear note with this name
+ • /removeallnotes*:* removes all notes from the group
  *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
  
 """
 
-__mod_name__ = "Notes"
+__mod_name__ = "🄽🄾🅃🄴🅂"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
