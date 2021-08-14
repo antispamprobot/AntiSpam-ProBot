897bedf6f0a92c3cb485043e45ea1cf494cda576
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-08-14 07:00:36 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "🄱🄰🄲🄺🅄🄿🅂"
+__mod_name__ = "|Backup|"
 
 __help__ = """
 *Only for group owner:*
