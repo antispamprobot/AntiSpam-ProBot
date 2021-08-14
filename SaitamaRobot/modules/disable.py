faf2988c3e8084310fb74d710e94b407fc9acbce
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-08-14 07:04:46 +0530

    Update disable.py

--- SaitamaRobot/modules/disable.py
+++ SaitamaRobot/modules/disable.py
@@ -338,7 +338,7 @@ if is_module_loaded(FILENAME):
     • /listcmds*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "🄲 🄳🄸🅂🄰🄱🄻🄰🄱🄻🄴"
+    __mod_name__ = "|Disable|"
 
 else:
     DisableAbleCommandHandler = CommandHandler
