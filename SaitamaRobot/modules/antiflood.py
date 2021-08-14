29a29564747d8192737edf236800090dbfa90c9d
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-08-14 06:59:23 +0530

    Update antiflood.py

--- SaitamaRobot/modules/antiflood.py
+++ SaitamaRobot/modules/antiflood.py
@@ -362,7 +362,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "🄰🄽🅃🄸🄵🄻🄾🄾🄳"
+__mod_name__ = "|AntiFlood|"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
