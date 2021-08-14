6217aa1cee1cf473d0e69fc8d3a5f1d4d3608994
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-08-14 13:04:48 +0530

    Update locks.py

--- SaitamaRobot/modules/locks.py
+++ SaitamaRobot/modules/locks.py
@@ -583,7 +583,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "🄻🄾🄲🄺🅂"
+__mod_name__ = "|Locks|"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
