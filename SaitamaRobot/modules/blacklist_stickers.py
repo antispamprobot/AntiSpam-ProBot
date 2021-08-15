6d64780cae1e0fc7ba87eda2745f54db5af153fd
DEVILTG <82862124+DEVILTG@users.noreply.github.com>
2021-08-15 13:08:58 +0530

    Update blacklist_stickers.py

--- SaitamaRobot/modules/blacklist_stickers.py
+++ SaitamaRobot/modules/blacklist_stickers.py
@@ -476,7 +476,7 @@ Note:
  • <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message.
 """
 
-__mod_name__ = "🄱 🅂🅃🄸🄲🄺🄴🅁"
+__mod_name__ = "|B Sticekrs|"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True)
